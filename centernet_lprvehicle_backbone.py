5feae5cc8699f9fbc619eed40838f3e5737cd4cf
s-vakhreev <svakhreev@gmail.com>
2019-11-12 14:49:54 +0300

    Bags baseline

---
+++ centernet_lprvehicle_backbone.py
@@ -0,0 +1,176 @@
+from pathlib import Path
+# model settings
+model = dict(
+    type='CenterNet',
+    pretrained='',
+    backbone=dict(
+        type='ConvnetLprVehicle',
+        out_indices=(1, 2, 3, 4)),
+    neck=None,
+    bbox_head=dict(
+        type='CenternetDetectionHead',
+        require_upsampling=True,
+        inplanes=(32, 64, 128, 256),
+        planes=(256, 128, 64),
+        base_down_ratio=32,
+        hm_head_conv=128,
+        hm_offset_heads_conv=128,
+        wh_heads_conv=128,
+        with_deformable=False,
+        hm_head_conv_num=2,
+        hm_offset_head_conv_num=2,
+        wh_head_conv_num=2,
+        num_classes=2,
+        shortcut_kernel=3,
+        norm_cfg=dict(type='BN'),
+        shortcut_cfg=(1, 2, 3),
+        num_stacks=1,  # It can be > 1 in backbones such as hourglass
+        ellipse_gaussian=True,
+        exp_wh=True,
+        hm_weight=1.,
+        hm_offset_weight=1.,
+        wh_weight=1.,
+        max_objs=128))
+cudnn_benchmark = True
+# training and testing settings
+train_cfg = dict(
+    vis_every_n_iters=100,
+    debug=False)
+test_cfg = dict(
+    score_thr=0.01,
+    max_per_img=100)
+# dataset settings
+albu_train_transforms = [
+    dict(
+        type='ShiftScaleRotate',
+        shift_limit=0.15,
+        scale_limit=0.15,
+        rotate_limit=10,
+        interpolation=1,
+        p=0.5),
+    dict(
+        type='RandomBrightnessContrast',
+        brightness_limit=[0.1, 0.3],
+        contrast_limit=[0.1, 0.3],
+        p=0.2),
+    dict(type='RandomShadow'),
+    dict(
+        type='OneOf',
+        transforms=[
+            dict(
+                type='RGBShift',
+                r_shift_limit=20,
+                g_shift_limit=20,
+                b_shift_limit=20,
+                p=1.0),
+            dict(
+                type='HueSaturationValue',
+                hue_shift_limit=10,
+                sat_shift_limit=15,
+                val_shift_limit=10,
+                p=1.0)
+        ],
+        p=0.2),
+    dict(type='JpegCompression', quality_lower=50, quality_upper=100, p=0.2),
+    dict(
+        type='OneOf',
+        transforms=[
+            dict(type='Blur', blur_limit=1, p=1.0),
+            dict(type='MedianBlur', blur_limit=1, p=1.0),
+        ],
+        p=0.2),
+    dict(
+        type='CLAHE',
+        p=0.2),
+    dict(
+        type='ToGray',
+        p=0.2),
+]
+dataset_type = 'DsslDataset'
+img_norm_cfg = dict(
+    mean=[0., 0., 0.], std=[255., 255., 255.], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(160, 128), keep_ratio=True),
+    dict(type='Pad', size=(160, 160), pad_val=0.5),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(
+        type='Albu',
+        transforms=albu_train_transforms,
+        bbox_params=dict(
+            type='BboxParams',
+            format='pascal_voc',
+            label_fields=['gt_labels'],
+            min_visibility=0.0,
+            filter_lost_elements=True),
+        keymap={
+            'img': 'image',
+            'gt_bboxes': 'bboxes'
+        }),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(160, 128),
+        flip=False,
+        transforms=[
+            dict(type='Resize', img_scale=(160, 128), keep_ratio=True),
+            dict(type='Pad', size=(160, 160), pad_val=0.5),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    imgs_per_gpu=64,
+    workers_per_gpu=8,
+    train=dict(
+        type=dataset_type,
+        ann_file='./dssl_bags_loader.py',
+        load_and_dump_config_name='load_and_dump_train_config',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file='./dssl_bags_loader.py',
+        load_and_dump_config_name='load_and_dump_test_config',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file='./dssl_bags_loader.py',
+        load_and_dump_config_name='load_and_dump_test_config',
+        pipeline=test_pipeline))
+# optimizer
+optimizer = dict(type='RAdam', lr=7e-4)
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=300,
+    warmup_ratio=1.0 / 3,
+    step=[90, 120])
+checkpoint_config = dict(interval=10)
+# runtime settings
+total_epochs = 140
+device_ids = range(8)
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+work_dir = './work_dirs/centernet_bags'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+# Need to import for inject hack registering
+from detector_utils.pytorch.utils.mmdet_wandb_hook import WandbLoggerHook
+log_config = dict(
+    interval=10,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='WandbLoggerHook', project='Centernet bags detector',
+             config_filename=Path.absolute(Path(__file__)))
+    ])
---
+++ dssl_bags_loader.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+from pathlib import Path
+
+from detector_utils import *
+
+__all__ = ['load_and_dump_train_config', 'load_and_dump_test_config', 'composer_config']
+
+categories = {0: 'item'}
+train_paths = [Path('/mnt/nfs/input/saved_temporary_data/bag_counter/bags_edge_v_8/release/train'),
+               Path('/mnt/nfs/input/saved_temporary_data/bag_counter/bags_edge_v_8/release/val')]
+test_paths = [Path('/mnt/nfs/input/saved_temporary_data/bag_counter/bags_edge_v_8/release/test')]
+
+
+def _create_load_dump_config(dataset_paths, prefix, clone_images=True):
+    return {
+        'categories': categories,
+
+        'data_loader': LegacyPickleLoadInformation(
+            data_paths=[DataPathInformation(path=p) for p in dataset_paths]
+        ),
+
+        'dump': DumpConfig(
+            clone_images=clone_images,
+            annotations_dump_filename=Path(f'data_{prefix}/annotations/annotation'),
+            images_clone_path=Path(f'data_{prefix}/images'))
+    }
+
+
+load_and_dump_train_config = _create_load_dump_config(dataset_paths=train_paths, prefix='train', clone_images=True)
+load_and_dump_test_config = _create_load_dump_config(dataset_paths=test_paths, prefix='test', clone_images=True)
+
+composer_config = {
+    'filters': [
+        {'type': 'ImageValidityFilter'},
+        {'type': 'ImageSizeFilter',
+         'min_size': Size2D(width=32, height=32),
+         'max_size': Size2D(width=10000, height=10000)},
+        {'type': 'BboxAbsoluteSizeFilter',
+         'min_size': Size2DF(width=0.01, height=0.01),
+         'max_size': Size2DF(width=1., height=1.)},
+    ],
+
+    'sampler': {'type': 'RandomSampler'}
+}
+
+if __name__ == '__main__':
+    load_and_dump(load_and_dump_config=load_and_dump_train_config)
+    load_and_dump(load_and_dump_config=load_and_dump_test_config)
+    # create_composer_and_debug(load_and_dump_configs=load_and_dump_train_config, composer_config=composer_config)
