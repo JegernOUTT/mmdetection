6959e0f59fcf1e050578910ff6c4cb13dc06beb8
s-vakhreev <svakhreev@gmail.com>
2019-11-15 00:42:26 +0300

    2x schedule reduced BS for better generalization

--- centernet_lprvehicle_backbone.py
+++ centernet_lprvehicle_backbone.py
@@ -160,7 +160,7 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    imgs_per_gpu=64,
+    imgs_per_gpu=16,
     workers_per_gpu=8,
     train=dict(
         type=dataset_type,
@@ -178,7 +178,7 @@ data = dict(
         load_and_dump_config_name='load_and_dump_test_config',
         pipeline=test_pipeline))
 # optimizer
-optimizer = dict(type='RAdam', lr=0.0005)
+optimizer = dict(type='RAdam', lr=2e-4)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
@@ -186,10 +186,10 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=300,
     warmup_ratio=1.0 / 3,
-    step=[60, 65])
+    step=[100, 120])
 checkpoint_config = dict(interval=10)
 # runtime settings
-total_epochs = 70
+total_epochs = 140
 device_ids = range(8)
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
