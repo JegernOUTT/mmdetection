5a6f62f2391b0b7c8d0acb172c6102bdf6169c21
s-vakhreev <svakhreev@gmail.com>
2019-12-24 14:45:28 +0300

    TTfnet support for augmix js loss

--- mmdet/models/detectors/ttfnet.py
+++ mmdet/models/detectors/ttfnet.py
@@ -1,3 +1,4 @@
+from .augmix_detector import AbstractAugmixDetector
 from .single_stage import SingleStageDetector
 from ..registry import DETECTORS
 
@@ -14,3 +15,21 @@ class TTFNet(SingleStageDetector):
                  pretrained=None):
         super(TTFNet, self).__init__(backbone, neck, bbox_head, train_cfg,
                                      test_cfg, pretrained)
+
+
+@DETECTORS.register_module
+class AugmixTTFNet(AbstractAugmixDetector):
+
+    def __init__(self,
+                 backbone,
+                 neck=None,
+                 bbox_head=None,
+                 train_cfg=None,
+                 test_cfg=None,
+                 pretrained=None):
+        super(AugmixTTFNet, self).__init__(backbone, neck, bbox_head, train_cfg,
+                                           test_cfg, pretrained)
+
+    def get_objectness_tensor_by_bboxhead_output(self, x):
+        return x[0]
+
